552a457bd96ae783402aaf9a6235cf97dbbcd858
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2018-06-06 21:50:46 +0200

    Nearest scale

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Sprite, Stage } from 'react-pixi-fiber';
 
 const TILE_SIZE = 32;
 
-const charactersBaseTexture = PIXI.Texture.fromImage(characters);
+const charactersBaseTexture = PIXI.Texture.fromImage(characters, true, PIXI.SCALE_MODES.NEAREST);
 
 const App = () => (
   <Stage width={800} height={600} options={{ backgroundColor: 0x10bb99 }}>
